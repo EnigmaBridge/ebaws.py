a125511954f574a4f384ed605828fcc3f7d5594f
Dusan Klinec <dusan.klinec@gmail.com>
2017-01-15 17:57:55 +0100

    pip fixes, v0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.1.3'
+version = '0.1.4'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
@@ -56,10 +56,11 @@ setup(
     name='ebaws.py',
     version=version,
     description='Enigma Bridge Python Utilities for AWS',
-    url='https://enigmabridge.com',
+    url='https://github.com/EnigmaBridge/ebaws.py',
     author="Enigma Bridge",
     author_email='info@enigmabridge.com',
-    license='MIT',
+    license=open('LICENSE').read(),
+    long_description=open('README.rst').read(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
